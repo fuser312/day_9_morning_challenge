4b83fe3b5192ea9d50064dcd1a8d2db63f724918
Sahil Singh <sahil.dogstar@gmail.com>
2019-10-11 18:02:19 +0530

    Finished Day9 Morning Challenge.

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,13 @@
-main() {
+//import 'dart:indexed_db';
 
+main() {
+  print(uniqueSort([6, 7, 3, 2, 1]));
+  print(uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]));
+
+  print(sumOfEvens([[1, 0, 2],[5, 5, 7],[9, 4, 3]]));
+
+  print(unmix("hTsii  s aimex dpus rtni.g"));
+  print(unmix("badce"));
 }
 
 // Challenge 1 - Purge and Organize
@@ -11,6 +19,20 @@ main() {
 // uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) ➞ [1, 2, 3, 4]
 // uniqueSort([6, 7, 3, 2, 1]) ➞ [1, 2, 3, 6, 7]
 
+List uniqueSort(List list) {
+  List newList = [];
+  for(int i = 0; i < list.length; i++){
+    if(newList.contains(list[i])){
+      continue;
+    }
+    else{
+      newList.add(list[i]);
+    }
+
+  }
+  newList.sort();
+  return newList;
+}
 
 // Challenge 2 - Sum of all Even Numbers in a Matrix
 // Create a function that returns the sum of all even elements in a 2D matrix.
@@ -40,6 +62,18 @@ main() {
 // ]) ➞ 0
 
 
+int sumOfEvens(List list){
+  int sum = 0;
+  for(int i =  0; i < list.length; i++){
+    for(int j = 0; j < list[i].length; j++){
+      if((list[i][j])%2 == 0){
+        sum = sum + list[i][j];
+      }
+    }
+  }
+  return sum;
+}
+
 // Challenge 3 - Unmix My Strings
 // 'lPaeesh le pemu mnxit ehess rtnisg!' Oh, sorry, that was supposed to say: Please help me unmix these strings!
 // Somehow my strings have all become mixed up; every pair of characters has been swapped.
@@ -50,3 +84,15 @@ main() {
 // unmix("hTsii  s aimex dpus rtni.g") ➞ "This is a mixed up string."
 //
 // unmix("badce") ➞ "abcde"
+
+String unmix(String string){
+  String x =  "";
+  for(int i = 0; i< string.length-1; i = i+2){
+    x = x + string[i+1] + string[i];
+  }
+
+  if(string.length%2 != 0){
+    x = x + string[string.length-1];
+  }
+  return(x);
+}
